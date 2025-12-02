3b73f5138aa284fa7cd8bef88bdc01d2a1398011
tranhaidangdz <141541516+tranhaidangdz@users.noreply.github.com>
2025-12-02 14:56:28 +0700

    thêm màn spash và spash(night mode): màn này khi người dùng chọn darkmode trên thanh tiện ích của đt thì nó sẽ đc áp dụng -thêm 2 màn splash -gọi style của màn đó vào AndroidManifest -gọi ExtensionFunction installSplashScreen trong main

--- app/src/main/java/com/loc/newsapp/MainActivity.kt
+++ app/src/main/java/com/loc/newsapp/MainActivity.kt
@@ -10,14 +10,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.loc.newsapp.ui.theme.NewsAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()  //khởi chạy splash screen trước khi chạy app chính(đây là 1 extension funtion của android)
         setContent {
             NewsAppTheme {
-
             }
         }
     }
